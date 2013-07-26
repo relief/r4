e170960ee1b454488b615df6c5775a4365a28a24
relief <zjuxie@gmail.com>
2013-07-25 21:34:31 -0700

    Try some of Renren Api

--- app/controllers/renren_controller.rb
+++ app/controllers/renren_controller.rb
@@ -1,4 +1,5 @@
 class RenrenController < ApplicationController
+  include RenrenApi
   require 'oauth2'
   require 'json'
   require 'net/http'
@@ -6,25 +7,27 @@ class RenrenController < ApplicationController
   require 'open-uri'
   require 'openssl'
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+  
 
     def client
-      OAuth2::Client.new(api_key,api_secret,:site => {
+      OAuth2::Client.new(Api_key,Api_secret,:site => {
         :url=>'https://graph.renren.com',
         :response_type=>'code'},
 	:access_token_url => 'https://graph.renren.com/oauth/token')
     end
     def login
 	redirect_to client.authorize_url(
- 	   :client_id    => client_id,
-	   :redirect_uri => redirect_uri,
-	   :response_type=> 'code'
+ 	   :client_id    => ClientID,
+	   :redirect_uri => Redirect_uri,
+	   :response_type=> 'code',
+	   :scope => 'read_user_feed'
 	)		
     end
     def loginnext
         @access_token = client.get_token({
-	      :client_id => api_key,
-	      :client_secret => api_secret,
-	      :redirect_uri => redirect_uri,
+	      :client_id => Api_key,
+	      :client_secret => Api_secret,
+	      :redirect_uri => Redirect_uri,
 	      :code => params[:code],
 	      :grant_type => "authorization_code"
 	})
@@ -44,68 +47,36 @@ class RenrenController < ApplicationController
       
 	redirect_to "/renren/user"#定向到我们的应用页面         
     end
+    def info
+	session_key = session[:renren_session_key]
+    end
     def user         
 	session_key=session[:renren_session_key]
- 
-	str="api_key=#{api_key}"         
-         
-	str<<"call_id=#{Time.now.to_i}"         
-         
-	str<<"format=JSON"         
-         
-	str<<"method=xiaonei.users.getInfo"         
-         
-	str<<"session_key=#{session_key}"         
-         
-	str<<"v=1.0"         
-         
-	str<<"#{api_secret}"#注意这里的str是有顺序的，具体的要求请参考［4］         
-         
-	sig=Digest::MD5.hexdigest(str)#生成参数sig（这是个什么东东呢，人人的解释是“ 它是由当前请求参数和secretKey的一个MD5值”）         
-        
-	uri = URI.parse('https://api.renren.com/v2/user/login/get')
-	params = { 
-		:access_token => session[:renren_access_token]
-	}
+	# 获得名字，图片，id
+	uri = setRRApiUri("user_login_get")
+	params = setRRApiParam("user_login_get", "")
+	uri.query = URI.encode_www_form( params)
+	@user = (JSON Net::HTTP.get(uri))["response"]
+ 	@userId =@user["id"]	
+	# 获得用户基本数据
+	uri = setRRApiUri("user_get")
+	params = setRRApiParam("user_get", @userId)
 	uri.query = URI.encode_www_form( params )
-	
-	@user = Net::HTTP.get(uri)
-=begin
- Add params to URI
-#uri.query = URI.encode_www_form( params ) 
-#	query={:api_key=>api_key,
- #              :call_id=>Time.now.to_i,
-  #             :format=>'JSON',
-               :method=>'xiaonei.users.getInfo',
-              :session_key=>session_key,
-               :v=>'1.0',
-               :sig=>sig
-        }#按照api组织参数         
-        query1 = {
-		:access_token => session[:renren_access_token]  
-	}
-        @user=JSON Net::HTTP::Get.new("https://api.renren.com/v2/user/login/get",query1)
-	source = "#{ROOT_DIR}/#{project}"
-	@user = Net::HTTP.get_response("example.com","/?search=thing&format=json").body
-	#.get_response(URI.parse(URI.encode("https://api.renren.com/v2/user/login/get")),query1).body
-         
-         #请求数据并且转换成json数据         
-=end
-    end         
-    def client_id
-	  '238917'
-    end
-    def api_key
-	  'ef1c4b01abd34dd59e1123c8bf20bc15'
-    end
-    def api_secret
-	  'baf04a7ca0b34a9f849c103760c29b4b'
-    end
-    def redirect_uri 
-	 # "http://localhost:3000/renren/loginnext"
-	  "https://zju.heroku.com/renren/loginnext"
-    end
-  #  def access_token
-#	session[:renren_access_token]=access_token.token
- #   end
-end
+	@res  = (JSON Net::HTTP.get(uri))["response"]
+	# 获得主页信息
+	uri = setRRApiUri("profile_get")
+	params = setRRApiParam("profile_get", @userId)
+	uri.query = URI.encode_www_form( params )
+	@profile  = (JSON Net::HTTP.get(uri))["response"]
+	# 获得用户新鲜事
+	uri = setRRApiUri("feed_list")
+	params = setRRApiParam("feed_list", @userId)
+	uri.query = URI.encode_www_form( params )
+	@feed  = (JSON Net::HTTP.get(uri))["response"]
+	# 获得用户所有好友	
+	uri = setRRApiUri("friend_list")
+	params = setRRApiParam("friend_list", @userId)
+	uri.query = URI.encode_www_form( params )
+	@friend_list = (JSON Net::HTTP.get(uri))["response"]
+    end        
+end 
